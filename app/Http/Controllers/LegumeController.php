3974c8044c57217a61757760f0f629c9b52b8592
Victor Santana <134336920+VitalHugal@users.noreply.github.com>
2024-05-06 15:17:56 -0300

    .

---
+++ app/Http/Controllers/LegumeController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Legume;
+use Illuminate\Http\Request;
+
+class LegumeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Legume $legume)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Legume $legume)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Legume $legume)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Legume $legume)
+    {
+        //
+    }
+}
---
+++ database/migrations/2024_05_06_181502_create_legumes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('legumes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome', 50)->unique();
+            $table->longText('descrição', 150);
+            $table->string('imagem', 100)->comment('Imagem do legume');
+            $table->float('preço', 10,2);
+            $table->integer('Estoque');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('legumes');
+    }
+};
